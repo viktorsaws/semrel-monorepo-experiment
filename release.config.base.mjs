b6c06374c7d6ab76d361b4f2abfea54005d67326
Viktor Shcherba <vshcherb@amazon.com>
2024-12-11 14:56:44 +0100

    chore: disable semantic release git step

--- release.config.base.mjs
+++ release.config.base.mjs
@@ -11,9 +11,9 @@ export const releaseConfig = (packageName) => ({
         '@semantic-release/changelog',
         '@semantic-release/npm',
         '@semantic-release/github', // Creates new Github Release
-        ['@semantic-release/git', {
-            message: 'chore(release): ' + packageName + ' ${nextRelease.version} [skip ci]'
-        }]
+        // ['@semantic-release/git', {
+        //     message: 'chore(release): ' + packageName + ' ${nextRelease.version} [skip ci]'
+        // }]
     ],
     tagFormat: packageName + '/v${version}',
     dryRun: false,
