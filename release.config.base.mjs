3c22312e6cc95b6749a8c6aaa02b936dcdc9c7d3
Viktor Shcherba <vshcherb@amazon.com>
2024-12-11 15:37:13 +0100

    chore: add pull request creation step

--- release.config.base.mjs
+++ release.config.base.mjs
@@ -13,7 +13,13 @@ export const releaseConfig = (packageName) => ({
         '@semantic-release/github', // Creates new Github Release
         // ['@semantic-release/git', {
         //     message: 'chore(release): ' + packageName + ' ${nextRelease.version} [skip ci]'
-        // }]
+        // }][
+        ["semantic-release-github-pullrequest", {
+            'assets': ['CHANGELOG.md', 'package.json', '../package-lock.json'],
+            'baseRef': 'main',
+            'pullrequestTitle': 'chore(release): ' + packageName + ' ${nextRelease.version} [skip ci]'
+        }
+    ]
     ],
     tagFormat: packageName + '/v${version}',
     dryRun: false,
